6e0dd44ff040646d9a611f9a5225a577e2f92af6
Hemansh-X797 <kundansmishra@gmail.com>
2026-09-13 15:43:03 +0530

    feat(vulkan): update Vulkan FFM bindings for shader module creation and pipeline dispatch

--- hyperion-runtime-vulkan/src/main/java/io/hyperion/runtime/vulkan/ffm/VulkanFFM.java
+++ hyperion-runtime-vulkan/src/main/java/io/hyperion/runtime/vulkan/ffm/VulkanFFM.java
@@ -94,6 +94,88 @@ public final class VulkanFFM {
             "vkEnumerateInstanceVersion",
             FunctionDescriptor.of(JAVA_INT, ADDRESS));
 
+    // ---------------------------------------------------------------
+    // Phase 5: logical device, memory, buffers, descriptors, pipelines, commands
+    // ---------------------------------------------------------------
+
+    public static final MethodHandle vkCreateDevice = handle(
+            "vkCreateDevice", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyDevice = handle(
+            "vkDestroyDevice", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS));
+    public static final MethodHandle vkGetDeviceQueue = handle(
+            "vkGetDeviceQueue", FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, JAVA_INT, ADDRESS));
+    public static final MethodHandle vkGetPhysicalDeviceMemoryProperties = handle(
+            "vkGetPhysicalDeviceMemoryProperties", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS));
+
+    public static final MethodHandle vkCreateBuffer = handle(
+            "vkCreateBuffer", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyBuffer = handle(
+            "vkDestroyBuffer", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkGetBufferMemoryRequirements = handle(
+            "vkGetBufferMemoryRequirements", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkAllocateMemory = handle(
+            "vkAllocateMemory", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkFreeMemory = handle(
+            "vkFreeMemory", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkBindBufferMemory = handle(
+            "vkBindBufferMemory", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, JAVA_LONG));
+    public static final MethodHandle vkMapMemory = handle(
+            "vkMapMemory", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_LONG, JAVA_LONG, JAVA_INT, ADDRESS));
+    public static final MethodHandle vkUnmapMemory = handle(
+            "vkUnmapMemory", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS));
+
+    public static final MethodHandle vkCreateShaderModule = handle(
+            "vkCreateShaderModule", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyShaderModule = handle(
+            "vkDestroyShaderModule", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+
+    public static final MethodHandle vkCreateDescriptorSetLayout = handle(
+            "vkCreateDescriptorSetLayout", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyDescriptorSetLayout = handle(
+            "vkDestroyDescriptorSetLayout", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkCreatePipelineLayout = handle(
+            "vkCreatePipelineLayout", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyPipelineLayout = handle(
+            "vkDestroyPipelineLayout", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkCreateComputePipelines = handle(
+            "vkCreateComputePipelines", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyPipeline = handle(
+            "vkDestroyPipeline", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+
+    public static final MethodHandle vkCreateDescriptorPool = handle(
+            "vkCreateDescriptorPool", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyDescriptorPool = handle(
+            "vkDestroyDescriptorPool", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkAllocateDescriptorSets = handle(
+            "vkAllocateDescriptorSets", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkUpdateDescriptorSets = handle(
+            "vkUpdateDescriptorSets", FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, ADDRESS, JAVA_INT, ADDRESS));
+
+    public static final MethodHandle vkCreateCommandPool = handle(
+            "vkCreateCommandPool", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkDestroyCommandPool = handle(
+            "vkDestroyCommandPool", FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkAllocateCommandBuffers = handle(
+            "vkAllocateCommandBuffers", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS));
+    public static final MethodHandle vkBeginCommandBuffer = handle(
+            "vkBeginCommandBuffer", FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS));
+    public static final MethodHandle vkEndCommandBuffer = handle(
+            "vkEndCommandBuffer", FunctionDescriptor.of(JAVA_INT, ADDRESS));
+    public static final MethodHandle vkCmdBindPipeline = handle(
+            "vkCmdBindPipeline", FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, ADDRESS));
+    public static final MethodHandle vkCmdBindDescriptorSets = handle(
+            "vkCmdBindDescriptorSets",
+            FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, ADDRESS, JAVA_INT, JAVA_INT, ADDRESS, JAVA_INT, ADDRESS));
+    public static final MethodHandle vkCmdDispatch = handle(
+            "vkCmdDispatch", FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, JAVA_INT, JAVA_INT));
+
+    public static final MethodHandle vkQueueSubmit = handle(
+            "vkQueueSubmit", FunctionDescriptor.of(JAVA_INT, ADDRESS, JAVA_INT, ADDRESS, ADDRESS));
+    public static final MethodHandle vkQueueWaitIdle = handle(
+            "vkQueueWaitIdle", FunctionDescriptor.of(JAVA_INT, ADDRESS));
+    public static final MethodHandle vkDeviceWaitIdle = handle(
+            "vkDeviceWaitIdle", FunctionDescriptor.of(JAVA_INT, ADDRESS));
+
     /** Throws with a decoded, human-readable message if result != VK_SUCCESS. */
     public static void check(int vkResult, String op) {
         if (vkResult != VulkanConstants.VK_SUCCESS) {
